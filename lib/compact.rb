a93d36d5ea5fa74bee55ed9049569b503503fc06
Arun Prasaad <arunbear@cpan.org>
2018-01-05 17:24:24 +0000

    Some working code and tests

--- lib/compact.rb
+++ lib/compact.rb
@@ -1,5 +1,66 @@
+require 'duplicate'
 require "compact/version"
 
 module Compact
-  # Your code goes here...
+  class Violation < RuntimeError
+  end
+
+  class Modular
+    def spec
+        @spec
+    end
+
+    def initialize(arg)
+        @spec = arg
+    end
+
+    def bind(impl)
+        @impl_src = impl
+        @impl = Class.new do
+            include impl 
+        end
+        check_interface
+        add_contracts
+        return @impl
+    end
+
+    def check_interface
+        @spec[:object_does].keys.each do |k|
+            @impl.instance_method k
+        end
+    end
+
+    def add_contracts
+        @spec[:object_does].each do |k, v|
+            add_post_conditions(k, v[:postcond])
+            add_invariants(k)
+        end
+    end
+
+    def add_post_conditions(meth, spec)
+        spec ||= {}
+        spec.each do |desc, proc| 
+            orig_meth = :"orig_#{meth}"
+            @impl.send(:alias_method, orig_meth, meth)
+            impl_src = @impl_src
+            create_method @impl, meth do |*args|
+                old = duplicate(self)
+                result = self.send(orig_meth, *args)
+                proc.call(self, old, result) or 
+                    raise Violation, "Post condition '#{desc}' on #{impl_src}##{meth} failed."
+                result
+            end
+        end 
+    end
+
+    def add_invariants(meth)
+        spec = @spec[:invariant] || {}
+        spec.each do |desc, proc| 
+        end 
+    end
+
+    def create_method(a_class, name, &block)
+        a_class.send(:define_method, name, &block)
+    end
+  end
 end
--- test/compact_test.rb
+++ test/compact_test.rb
@@ -1,11 +1,54 @@
 require "test_helper"
+require 'boundedstack'
 
 class CompactTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Compact::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def setup
+    @contract = Compact::Modular.new(
+        object_does: {
+            push: {
+                postcond: {
+                    size_increased_by_one: proc { |obj, old|
+                        obj.size == old.size + 1
+                    }
+                }
+            },
+            pop: {
+                postcond: {
+                    returns_old_top: -> (obj, old, result) {
+                        result == old.top
+                    }
+                }
+            },
+            size: {},
+            top: {},
+        },
+        invariant: {
+            max_size_not_exceeded: -> (obj) { obj.size <= obj.max_size }
+        }
+    )
+  end
+
+  def test_bounded_stack
+    stack_class = @contract.bind(BoundedStack)
+    stack = stack_class.new
+    stack.push 1
+    stack.push 2
+    assert stack.size == 2
+    assert stack.pop == 2
+    assert stack.size == 1
+  end
+
+  def test_bounded_stack_bad_top
+    stack_class = @contract.bind(BoundedStackBadTop)
+    stack = stack_class.new
+    stack.push 1
+    stack.push 2
+    assert_raises Compact::Violation do
+      stack.pop == 2
+    end
   end
 end
